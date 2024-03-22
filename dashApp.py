242159c009606f4a9ae65c848a5eac60f46e37b4
Jeroen Lüling <jtube2014@gmail.com>
2024-03-22 15:03:35 +0100

    (DATABASE DISABLED)added sentiment analysis page on the dashApp.

--- dashApp.py
+++ dashApp.py
@@ -1,12 +1,15 @@
-from dash import Dash, html, dcc, callback, Output, Input, dash_table
+from dash import Dash, html, dcc, callback, Output, Input, State, dash_table
+
 import plotly.express as px
 import pandas as pd
 from sqlalchemy import create_engine
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+from sentiment import analyze_sentiment  # Import the sentiment analysis method
 
-dbengine = create_engine('postgresql://postgres:root@localhost/test')
 
-dt = pd.read_sql('SELECT * FROM stuff.stuff', dbengine)
+#dbengine = create_engine('postgresql://postgres:root@localhost/test')
+
+#dt = pd.read_sql('SELECT * FROM stuff.stuff', dbengine)
 
 app = Dash("MovieDash", external_stylesheets=['./assets/navbar.css'])
 
@@ -15,40 +18,63 @@ pageB = html.Div(children=[
     dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
     html.Div(id='output-container', style={'display': 'flex'}, children=[
         dcc.Graph(id='graph-content', style={'width': '50%', 'height': '500px'}),
-        html.Div(children=[dash_table.DataTable(dt.to_dict('records'), page_size=300)], style={'width': '50%'}),
+        #html.Div(children=[dash_table.DataTable(dt.to_dict('records'), page_size=300)], style={'width': '50%'}),
     ])
+])
+
+pageC = html.Div(children=[
+    html.H1(children='Title of Dash App', style={'textAlign': 'center'}),
+    html.Div([
+        dcc.Input(id='keyword-input', type='text', placeholder='Enter keyword'),
+        html.Button('Analyze Sentiment', id='analyze-button', n_clicks=0)
+    ]),
+    html.Div(id='sentiment-results'),
+    html.Div(id='keyword-input-previous', style={'display': 'none'})  # Hidden div to store previous keyword value
 ])
 
 pages = {
     '/': {'name': 'Home', 'content': html.Div(children="Dit is de homepagina")},
     '/page1': {'name': 'PageA', 'content': html.Div(children="Dit is pagina A")},
     '/page2': {'name': 'PageB', 'content': pageB},
-    '/page3': {'name': 'PageC', 'content': html.Div(children="Dit is pagina C")},
+    '/page3': {'name': 'PageC', 'content': pageC},
 }
 
+# Define app layout
 app.layout = html.Div(children=[
     dcc.Location(id='url'),
     html.Div(className='navbar', children=[html.A(href=path, children=page['name']) for path, page in pages.items()]),
     html.Div(id='page-content')
 ])
 
-
+# Callback to display page based on URL pathname
 @app.callback(
-    Output(component_id='page-content', component_property='children'),
-    Input(component_id='url', component_property='pathname')
+    Output('page-content', 'children'),
+    Input('url', 'pathname')
 )
 def display_page(pathname):
     return pages.get(pathname, {'content': html.Div(children="404")})['content']
 
-
-@callback(
-    Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
+# Callback to update sentiment results
+@app.callback(
+    Output('sentiment-results', 'children'),
+    Output('keyword-input-previous', 'children'),
+    Input('analyze-button', 'n_clicks'),
+    State('keyword-input', 'value'),
+    State('keyword-input-previous', 'children')
 )
-def update_graph(value):
-    dff = df[df.country == value]
-    return px.line(dff, x='year', y='pop')
-
+def update_sentiment(n_clicks, keyword, keyword_previous):
+    keyword_previous = keyword_previous or ''  # Set default value if previous state is not defined
+    # Check if button is clicked and keyword input value changes
+    if n_clicks and keyword != keyword_previous:
+        sentiment_results = analyze_sentiment(keyword)
+        return (
+            html.Div([
+                html.H3(f"Sentiment Analysis Results for '{keyword}'"),
+                html.P(sentiment_results)
+            ]),
+            keyword  # Update previous keyword value
+        )
+    return None, keyword_previous
 
 if __name__ == '__main__':
     app.run(debug=False)
--- sentiment.py
+++ sentiment.py
@@ -1,126 +1,120 @@
-# utilities
+# sentiment_analysis.py
+
 import numpy as np
 import pandas as pd
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
-#sentiment analysis
 from textblob import TextBlob
 import math
+import os
 
+# Get the directory of the current script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# Construct the absolute path to twitterdata.csv
+csv_path = os.path.join(current_dir, 'twitterdata.csv')
+def analyze_sentiment(datafilterkeyword: str):
+    # Importing the dataset and setting up pandas
+    pd.set_option('display.max_colwidth', 255)
+    DATASET_COLUMNS = ['target', 'id', 'date', 'flag', 'user', 'twitter message']
+    DATASET_ENCODING = "ISO-8859-1"
+    df = pd.read_csv(csv_path, encoding=DATASET_ENCODING, names=DATASET_COLUMNS)
+    del df['target']  # delete unused column
+    del df['user']  # delete unused column
+    del df['date']  # delete unused column
+    del df['flag']  # delete unused column
 
+    filtered_data = df[df['twitter message'].str.contains(datafilterkeyword)]  # filter data
 
-# Importing the dataset and setting up pandas
-pd.set_option('display.max_colwidth', 255)
-DATASET_COLUMNS=['target','id','date','flag','user','twitter message']
-DATASET_ENCODING = "ISO-8859-1"
-df = pd.read_csv('twitterdata.csv', encoding=DATASET_ENCODING, names=DATASET_COLUMNS)
-del df['target'] #delete unused collumn
-del df['user'] #delete unused collumn
-del df['date'] #delete unused collumn
-del df['flag'] #delete unused collumn
+    class Mood:
+        sentiment: float
 
-datafilterkeyword = " birthday " #keyword by which to filter data
-filtered_data = df[df['twitter message'].str.contains(datafilterkeyword)] #filter data
+        def __init__(self, sentiment: float):
+            self.sentiment = sentiment
 
+    def bubbleSort(arr):
+        n = len(arr)
+        for i in range(n):
+            for j in range(0, n - i - 1):
+                if arr[j] > arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
-class Mood: #class for the sentiment
-    sentiment: float
+    def get_mood(input_text: str, *, threshold: float, totalpositive: int, totalneutral: int, totalnegative: int) -> tuple[
+        Mood, int, int, int]:
+        sentiment: float = TextBlob(input_text).sentiment.polarity
 
-    def __init__(self, sentiment: float):
-        self.sentiment = sentiment
+        if sentiment >= threshold:
+            mood = Mood(sentiment)
+            totalpositive += 1
+        elif sentiment <= -threshold:
+            mood = Mood(sentiment)
+            totalnegative += 1
+        else:
+            mood = Mood(sentiment)
+            totalneutral += 1
 
-# Python3 program for Bubble Sort Algorithm Implementation source: https://www.geeksforgeeks.org/sorting-algorithms-in-python/
-def bubbleSort(arr):
-     
-    n = len(arr)
- 
-    # For loop to traverse through all 
-    # element in an array
-    for i in range(n):
-        for j in range(0, n - i - 1):
-             
-            # Range of the array is from 0 to n-i-1
-            # Swap the elements if the element found 
-            #is greater than the adjacent element
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-                 
+        return mood, totalpositive, totalneutral, totalnegative
 
-
-
-def get_mood(input_text: str, *, threshold: float, totalpositive: int, totalneutral: int, totalnegative: int) -> tuple[Mood, int, int, int]: #sentiment generator
-    sentiment: float = TextBlob(input_text).sentiment.polarity #generate the sentiment
-
-    if sentiment >= threshold: #catagorize the sentiment to positive, negative and neutral
-        mood = Mood(sentiment)
-        totalpositive += 1
-    elif sentiment <= -threshold:
-        mood = Mood(sentiment)
-        totalnegative += 1
-    else:
-        mood = Mood(sentiment)
-        totalneutral += 1
-
-    return mood, totalpositive, totalneutral, totalnegative
-
-
-if __name__ == '__main__':
-    #initiating variables
+    # Initiating variables
     resultMood = ''
-    totalsentiment = 0 #all sentiment added up
-    totalsentimentvalues= 0 #number of collected sentiment values
-    averagesentiment = 0 #total/collected
-    mediansentiment = 0 #the median
-    sentimentarray = [] #array with every collected value
-    totalpositive = 0 #positive values
-    totalneutral = 0 #neutral values
-    totalnegative = 0 #negative values
-    standarddeviation = 0 #standard deviation
-    threshold=0.15
-    
+    totalsentiment = 0
+    totalsentimentvalues = 0
+    averagesentiment = 0
+    mediansentiment = 0
+    sentimentarray = []
+    totalpositive = 0
+    totalneutral = 0
+    totalnegative = 0
+    standarddeviation = 0
+    threshold = 0.15
 
-for index, row in filtered_data.iterrows(): #for every row in the filtered data table
-    text: str = row['twitter message'] #set the input text to the indexed twitter message
-    mood, totalpositive, totalneutral, totalnegative = get_mood(text, threshold=threshold, totalpositive=totalpositive, totalneutral=totalneutral, totalnegative=totalnegative) #generate the sentiment values
+    for index, row in filtered_data.iterrows():
+        text: str = row['twitter message']
+        mood, totalpositive, totalneutral, totalnegative = get_mood(text, threshold=threshold,
+                                                                    totalpositive=totalpositive,
+                                                                    totalneutral=totalneutral,
+                                                                    totalnegative=totalnegative)
 
-    totalsentiment += mood.sentiment
-    totalsentimentvalues += 1
-    sentimentarray.append(mood.sentiment)
+        totalsentiment += mood.sentiment
+        totalsentimentvalues += 1
+        sentimentarray.append(mood.sentiment)
 
+    bubbleSort(sentimentarray)
 
-bubbleSort(sentimentarray) #sort the sentimentarray to get median
+    mediansentiment = round(sentimentarray[math.floor(len(sentimentarray) / 2)], 3)
+    averagesentiment = round(totalsentiment / totalsentimentvalues, 3)
+    standarddeviation = round(np.std(sentimentarray), 3)
+    if averagesentiment >= threshold:
+        resultMood = ':D'
+    elif averagesentiment <= -threshold:
+        resultMood = '>:('
+    else:
+        resultMood = ':|'
 
-mediansentiment = round(sentimentarray[math.floor(len(sentimentarray)/2)],3)
-averagesentiment = round(totalsentiment/totalsentimentvalues,3)
-standarddeviation = round(np.std(sentimentarray),3)
-if averagesentiment >= threshold: #catagorize the sentiment to positive, negative and neutral
-    resultMood = ':D'
-elif averagesentiment <= -threshold:
-    resultMood = '>:('
-else:
-    resultMood = ':|'
+    print("--- sentiment for '" + datafilterkeyword + "'---\naverage sentiment value:", averagesentiment, resultMood,
+          "\nmedian sentiment:", mediansentiment, "\nstandard deviation:", standarddeviation,
+          "\n# pos/neu/neg:", totalpositive, "/", totalneutral, "/", totalnegative)
 
-print("--- sentiment for '" + datafilterkeyword + "'---\naverage sentiment value:",averagesentiment,resultMood,"\nmedian sentiment:",mediansentiment,"\nstandard deviation:",standarddeviation,"\n# pos/neu/neg:",totalpositive,"/",totalneutral,"/",totalnegative)
+    # Unify Text from all weeks
+    text = filtered_data['twitter message'].str.cat(sep=' ')
 
+    # Set the stopwords list
+    stopwords = set(STOPWORDS)
+    new_words = ["feel", "feeling"]
+    new_stopwords = stopwords.union(new_words)
 
-# Unify Text from all weeks
-text = filtered_data['twitter message'].str.cat(sep=' ')
-     
+    # Size of Word Cloud
+    plt.rcParams["figure.figsize"] = (7, 4)
 
-#set the stopwords list
-stopwords= set(STOPWORDS)
-new_words = ["feel", "feeling"]
-new_stopwords=stopwords.union(new_words)
-     
+    # Make Wordcloud
+    wordcloud = WordCloud(max_font_size=50, max_words=80, background_color="white", stopwords=new_stopwords,
+                          colormap='jet').generate(text)
 
-# Size of Word Cloud
-plt.rcParams["figure.figsize"] = (7,4)
+    # Plot Wordcloud
+    plt.plot()
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
+    plt.show()
 
-# Make Wordcloud
-wordcloud = WordCloud(max_font_size=50, max_words=80, background_color="white",stopwords=new_stopwords, colormap='jet').generate(text)
-
-# Plot Wordcloud
-plt.plot()
-plt.imshow(wordcloud, interpolation="bilinear")
-plt.axis("off")
-plt.show()
+# Call the function with the desired datafilterkeyword
+if __name__ == '__main__':
+    analyze_sentiment("birthday")
